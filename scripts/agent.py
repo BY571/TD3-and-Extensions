fad141c84c95e0c37fa6a75e13bb9d7712732f4b
BY571 <sebastian.dittert@gmx.de>
2021-03-11 18:50:21 +0100

    add td3 base

---
+++ scripts/agent.py
@@ -0,0 +1,178 @@
+
+import torch
+import numpy as np
+import random
+from networks import Actor, Critic
+import torch.optim as optim
+import torch.nn.functional as F
+
+class TD3_Agent():
+    """Interacts with and learns from the environment."""
+    
+    def __init__(self,
+                 state_size,
+                 action_size,
+                 action_low,
+                 action_high,
+                 replay_buffer,
+                 batch_size,
+                 random_seed,
+                 lr,
+                 hidden_size,
+                 gamma,
+                 tau,
+                 device):
+        """Initialize an Agent object
+
+        Args:
+            state_size (int):    State size
+            action_size (int):   Action size
+            replay_buffer:       Experience Replay Buffer
+            batch_size:          Batch size when learning
+            random_seed (int):   Random seed
+            lr (float):          Learning rate
+            hidden_size (int):   Number of hidden units per layer
+            gamma (float):       Discount factor
+            tau (float):         Tau, soft-update parameter
+            device (torch device): Training Device cpu or cuda:0
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.action_low = action_low
+        self.action_high = action_high
+        self.seed = random.seed(random_seed)
+        
+        self.gamma = gamma
+        self.tau = tau
+        self.batch_size = batch_size
+                
+        print("Using: ", device)
+        self.device = device
+        self.iter = 1
+        
+        # Actor Network 
+        self.actor_local = Actor(state_size, action_size, random_seed, hidden_size=hidden_size).to(device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=lr)     
+        
+        # Critic Network (w/ Target Network)
+
+        self.critic1 = Critic(state_size, action_size, random_seed, hidden_size=hidden_size).to(device)
+        self.critic2 = Critic(state_size, action_size, random_seed+1, hidden_size=hidden_size).to(device)
+        self.target_critic1 = Critic(state_size, action_size, random_seed, hidden_size=hidden_size).to(device)
+        self.target_critic2 = Critic(state_size, action_size, random_seed+1, hidden_size=hidden_size).to(device)
+
+        self.optimizer1 = optim.Adam(self.critic1.parameters(), lr=lr, weight_decay=0)
+        self.optimizer2 = optim.Adam(self.critic2.parameters(), lr=lr, weight_decay=0)
+
+        # Replay memory
+        self.memory = replay_buffer
+        
+
+    def step(self, state, action, reward, next_state, done):
+        """Save experience in replay memory, and use random sample from buffer to learn."""
+        # Save experience / reward
+        self.memory.add(state, action, reward, next_state, done)
+
+
+        if len(self.memory) > self.batch_size:
+            experiences = self.memory.sample()
+            self.learn(experiences)
+            
+    
+    def act(self, state):
+        """Returns actions for given state as per current policy."""
+        state = torch.from_numpy(state).float().to(self.device)
+        mu = self.actor_local(state)
+        action = mu + torch.normal(mean=torch.FloatTensor([0.]),
+                                        std=torch.FloatTensor([0.1])).to(mu.device)
+        return action.detach().cpu()[0]
+    
+    def eval(self, state):
+        state = torch.from_numpy(state).float().to(self.device)
+        mu = self.actor_local(state)
+        return mu.detach().cpu()[0]
+    
+    def learn(self,experiences):
+        """Updates actor, critics and entropy_alpha parameters using given batch of experience tuples.
+        Q_targets = r + γ * (min_critic_target(next_state, actor_target(next_state)) - α *log_pi(next_action|next_state))
+        Critic_loss = MSE(Q, Q_target)
+        Actor_loss = α * log_pi(a|s) - Q(s,a)
+        where:
+            actor_target(state) -> action
+            critic_target(state, action) -> Q-value
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+       
+
+        # ---------------------------- update critic ---------------------------- #
+
+        with torch.no_grad():
+            # Get predicted next-state actions and Q values from target models
+            next_action = self.actor_local(next_states)
+            next_action = next_action + torch.clamp(
+                                                torch.normal(mean=torch.FloatTensor([0.]),
+                                                             std=torch.FloatTensor([0.2])),
+                                                min=-0.5, max=0.5).to(next_action.device)
+            next_action = torch.clamp(next_action, min=self.action_low, max=self.action_high)
+            # TODO: make this variable for possible more than two target critics
+            Q_target1_next = self.target_critic1(next_states, next_action.squeeze(0))
+            Q_target2_next = self.target_critic2(next_states, next_action.squeeze(0))
+            
+            # take the min of both critics for updating
+            Q_target_next = torch.min(Q_target1_next, Q_target2_next)
+
+        Q_targets = rewards.cpu() + (self.gamma * (1 - dones.cpu()) * Q_target_next.cpu())
+
+        # Compute critic losses and update critics 
+
+        Q1 = self.critic1(states, actions).cpu()
+        Q2 = self.critic2(states, actions).cpu()
+        Q1_loss = 0.5*F.mse_loss(Q1, Q_targets)
+        Q2_loss = 0.5*F.mse_loss(Q2, Q_targets)
+    
+        # Update critic
+        self.optimizer1.zero_grad()
+        self.optimizer2.zero_grad()
+        Q1_loss.backward()
+        Q2_loss.backward()
+        self.optimizer1.step()
+        self.optimizer2.step()
+
+
+        # ---------------------------- update actor ---------------------------- #
+        if self.iter % 2 == 0:
+
+            actions_pred = self.actor_local(states)             
+            
+            Q1 = self.critic1(states, actions_pred.squeeze(0)).cpu()
+            #Q2 = self.critic2(states, actions_pred.squeeze(0)).cpu()
+            #Q = torch.min(Q1,Q2)
+
+            actor_loss =  -(Q1).mean() # * weights
+            # Optimize the actor loss
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+            
+            # soft update of the targets
+            self.soft_update(self.critic1, self.target_critic1)
+            self.soft_update(self.critic2, self.target_critic2)
+            self.iter = 0
+
+        self.iter +=1
+    
+    def soft_update(self, local_model, target_model):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        Params
+        ======
+            local_model: PyTorch model (weights will be copied from)
+            target_model: PyTorch model (weights will be copied to)
+            tau (float): interpolation parameter 
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(self.tau*local_param.data + (1.0-self.tau)*target_param.data)
---
+++ train.py
@@ -0,0 +1,169 @@
+
+
+from scripts.buffer import ReplayBuffer
+import gym
+import random
+import numpy as np 
+import torch 
+import pybullet_envs # to run e.g. HalfCheetahBullet-v0 different reward function bullet-v0 starts ~ -1500. pybullet-v0 starts at 0
+from collections import deque
+import time
+from torch.utils.tensorboard import SummaryWriter
+import argparse
+import json
+from scripts.agent import TD3_Agent
+
+def timer(start,end):
+    """ Helper to print training time """
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("\nTraining Time:  {:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
+
+def evaluate(step, eval_runs=5, capture=False):
+    """
+    Makes an evaluation run with the current epsilon
+    """
+
+    reward_batch = []
+    for i in range(eval_runs):
+        state = eval_env.reset()
+
+        rewards = 0
+        while True:
+            action = agent.eval(np.expand_dims(state, axis=0))
+            action_v = np.clip(action, action_low, action_high)
+            state, reward, done, _ = eval_env.step(action_v)
+            rewards += reward
+            if done:
+                break
+        reward_batch.append(rewards)
+    if capture == False:   
+        writer.add_scalar("Test_Reward", np.mean(reward_batch), step)
+
+def fill_buffer(agent, env, samples=1000):
+    collected_samples = 0
+    
+    state = env.reset()
+    state = state.reshape((1, state_size))
+    for i in range(samples):
+            
+        action = env.action_space.sample()
+        next_state, reward, done, info = env.step(action)
+        next_state = next_state.reshape((1, state_size))
+        agent.memory.add(state, action, reward, next_state, done)
+        collected_samples += 1
+        state = next_state
+        if done:
+            state = env.reset()
+            state = state.reshape((1, state_size))
+    print("Added random samples to the Buffer - Buffer size: ", agent.memory.__len__())
+
+    
+def train(args):
+    scores_deque = deque(maxlen=100)
+    average_100_scores = []
+    scores = []
+    i_episode = 1
+    state = env.reset()
+    state = state.reshape((1, state_size))
+    score = 0
+    steps = args.steps
+    for step in range(1, steps+1):
+        # eval runs
+        if step % args.eval_every == 0 or step == 1:
+            evaluate(step, args.eval_runs)
+
+
+        action = agent.act(state)
+        action_v = action.numpy()
+        action_v = np.clip(action_v, action_low, action_high)
+        next_state, reward, done, info = env.step(action_v)
+        next_state = next_state.reshape((1, state_size))
+        agent.step(state, action, reward, next_state, done)
+        
+        state = next_state
+        score += reward
+
+        if done:
+            scores_deque.append(score)
+            scores.append(score)
+            average_100_scores.append(np.mean(scores_deque))
+            writer.add_scalar("Average100", np.mean(scores_deque), step)
+            writer.add_scalar("Train_Reward", score, step)
+            state = env.reset()
+            state = state.reshape((1, state_size))
+            
+            print('\rEpisode {} Env. Step: [{}/{}] Train-Reward: {:.2f}  Average100 Score: {:.2f} '.format(i_episode, step, steps, score, np.mean(scores_deque)), end="")
+            if i_episode % args.print_every == 0:
+                print('\rEpisode {} Env. Step: [{}/{}] Train-Reward: {:.2f}  Average100 Score: {:.2f}'.format(i_episode, step, steps, score, np.mean(scores_deque)))
+            score = 0 
+            i_episode += 1
+
+        
+    return np.mean(scores_deque)
+
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--env", type=str,default="HalfCheetahBulletEnv-v0", help="Environment name, default = HalfCheetahBulletEnv-v0")
+parser.add_argument("--info", type=str, default="TD3-training-run-1", help="Information or name of the run")
+parser.add_argument("--steps", type=int, default=1_000_000, help="The amount of training interactions with the environment, default is 1mio")
+parser.add_argument("--collect_random", type=int, default=5_000, help="Collect transitions of the envrionment with a random policy before training, default is 5000 transitions")
+parser.add_argument("--eval_every", type=int, default=10_000, help="Number of interactions after which the evaluation runs are performed, default = 10.000")
+parser.add_argument("--eval_runs", type=int, default=1, help="Number of evaluation runs performed, default = 1")
+parser.add_argument("--seed", type=int, default=0, help="Seed for the env and torch network weights, default is 0")
+parser.add_argument("--lr", type=float, default=3e-4, help="Actor learning rate of adapting the network weights, default is 3e-4")
+parser.add_argument("--layer_size", type=int, default=256, help="Number of nodes per neural network layer, default is 256")
+parser.add_argument("--replay_memory", type=int, default=int(1e6), help="Size of the Replay memory, default is 1e6")
+parser.add_argument("-bs", "--batch_size", type=int, default=256, help="Batch size, default is 256")
+parser.add_argument("-t", "--tau", type=float, default=0.005, help="Softupdate factor tau, default is 0.005")
+parser.add_argument("-g", "--gamma", type=float, default=0.99, help="discount factor gamma, default is 0.99")
+parser.add_argument("--print_every", type=int, default=100, help="Print recent training results every x epochs, defaut is 100")
+
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    
+
+    writer = SummaryWriter("runs/"+args.info)
+    env = gym.make(args.env)
+    eval_env = gym.make(args.env)
+    action_high = env.action_space.high[0]
+    seed = args.seed
+    action_low = env.action_space.low[0]
+    torch.manual_seed(seed)
+    env.seed(seed)
+    eval_env.seed(seed+1)
+    np.random.seed(seed)
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.shape[0]
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    replay_buffer = ReplayBuffer(action_size, args.replay_memory, args.batch_size, seed, device)
+    agent = TD3_Agent(state_size=state_size,
+                action_size=action_size,
+                action_low=action_low,
+                action_high=action_high,
+                replay_buffer=replay_buffer,
+                batch_size=args.batch_size,
+                random_seed=seed,
+                lr=args.lr,
+                hidden_size=args.layer_size,
+                gamma=args.gamma,
+                tau=args.tau,
+                device=device)
+
+    fill_buffer(agent, env=env, samples=args.collect_random)
+    
+    t0 = time.time()
+    final_average100 = train(args)
+    t1 = time.time()
+    env.close()
+    timer(t0, t1)
+    
+    # save parameter
+    with open('runs/'+args.info+".json", 'w') as f:
+        json.dump(args.__dict__, f, indent=2)
+    hparams = vars(args)
+    metric = {"final average 100 train reward": final_average100}
+    writer.add_hparams(hparams, metric)
